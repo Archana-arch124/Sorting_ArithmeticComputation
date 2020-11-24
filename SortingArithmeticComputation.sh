051243bbc749e0c57a92b3676e1d208a19ae913e
unknown <#archgowda124@gmail.com>
2020-11-24 21:00:56 +0530

    Input 3 values

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -1,2 +1,5 @@
-Welcome to Sorting Arithmetic computation problem
+#Welcome to Sorting Arithmetic computation problem
+read -p " enter input1 :" a
+read -p " enter input2 :" b
+read -p " enter input3 :" c
 
