091d948aabca7a5589e939fc69de08b1b79eb011
unknown <#archgowda124@gmail.com>
2020-11-24 21:21:20 +0530

    Result in dictionary to array

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -17,3 +17,5 @@ compute["result"]=$result3
 compute["result"]=$result4
 
 arr=( [result1]=$result1 [result2]=$result2 [result3]=$result3 [result4]=$result4 )
+echo "All values stored in a dictionary:${arr[@]}"
+
